3ff218a456a68be857d2aec83983cb95f7783056
AyamBercakap <bercakapayam@gmail.com>
2025-03-25 01:48:58 +0800

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,13 +1,17 @@
 function openTab(evt, TabName) {
   var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.display = 'none';
   }
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
-  document.getElementById(TabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(TabName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+  
+  // Trigger scramble effect on the newly active tab
+  const scrambler = new TextScramble(evt.currentTarget);
+  scrambler.scrambleOnActive();
 }
